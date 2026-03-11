770b5551ae4e9724248cd7dda24537c130c56442
Gabriel Teixeira <gabriel.silveira.dev@gmail.com>
2026-03-11 16:12:12 -0300

    Initial commit On branch master new file:   .gitattributes new file:   .gitignore new file:   .mvn/wrapper/maven-wrapper.properties new file:   Dockerfile new file:   compose.yaml new file:   mvnw new file:   mvnw.cmd new file:   pom.xml new file:   src/main/java/io/github/gabrieltxrs/agenda/AgendaApplication.java new file:   src/main/resources/application.properties new file:   src/test/java/io/github/gabrieltxrs/agenda/AgendaApplicationTests.java

---
+++ src/test/java/io/github/gabrieltxrs/agenda/AgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gabrieltxrs.agenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
